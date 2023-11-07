4cee02a58d41b8ac064cac51dea44863a390d0d1
GaniFatur <1207050134@student.uinsgd.ac.id>
2023-11-07 14:34:09 +0700

    edit and delete

--- pages/lib/db.js
+++ pages/lib/db.js
@@ -8,6 +8,13 @@ const con = mysql.createConnection({
   database: 'nextjs_crud',
 });
 
+// const con = mysql.createConnection({
+//   host: 'sql206.infinityfree.com',
+//   user: 'if0_35378913',
+//   password: 'UkzzjpkXx0I0',
+//   database: 'if0_35378913_kkndev',
+// });
+
 con.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
