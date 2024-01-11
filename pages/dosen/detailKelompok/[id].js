b9e847e50eb304e72e735647f05c24ae7ff68f81
muhamadiqbalsetiawan <muhamadsetiawan031@gmail.com>
2024-01-11 09:35:46 +0700

    iqbal 11/01/24 09:35

--- pages/dosen/detailKelompok/[id].js
+++ pages/dosen/detailKelompok/[id].js
@@ -22,155 +22,99 @@ export default function DetailKelompok() {
             <div className='absolute my-2 mx-4 md:my-6 md:mx-6 bg-white p-2 rounded-full drop-shadow-xl z-40'>
                 <Link href='/dosen/kelompok' className="text-xl"><IoChevronBackOutline /></Link>
             </div>
-            <Navbar />
-            <div className="mt-14 mb-2 ml-6 md:ml-10 md:mt-6 md:mb-10 font-bold text-2xl md:text-5xl text-white flex justify-center">
+            {/* <Navbar /> */}
+            <div className="mt-8 mb-2 ml-6 md:ml-10 md:mt-6 md:mb-10 font-bold text-2xl md:text-5xl text-white flex justify-center">
               <h1>Kelompok Binaan</h1>
             </div>
-            <div className="p-3 mx-4 md:mt-8 md:mx-20 md:p-6 bg-iceGray rounded-xl md:w-2/3">
-                <h1 class="text-xl font-bold text-gray-900 dark:text-white flex justify-center items-center">
-                    {namakelompok}
-                </h1>
-                <div className="md:flex md:justify-center md:items-center">
+            <div className="md:flex md:justify-center md:items-center">
+                <div className="p-3 md:py-4 md:px-8 mx-4  bg-iceGray rounded-xl md:w-auto">
+                  <h1 class="text-xl font-bold text-gray-900 dark:text-white flex justify-center items-center">
+                      {namakelompok}
+                  </h1>
                     <div class=" pb-2 text-left">
-                        <div className="mb-2">
+                        <div className="mb-2 text-sm font-semibold md:text-lg md:font-semibold">
                             <h3>Ketua : {ketua}</h3>
                             <h3>Kontak Ketua : {telp}</h3>
                             <h3>Lokasi : Desa Cipaku, Kecamatan Paseh, Kabupaten Bandung, Jawa Barat</h3>
                             <h3>Anggota :</h3>
                         </div>
-                        <div className='bg-white grid justify-center items-center rounded-xl md:mx-9 md:w-auto'>
+                        <div className='md:mx-9 md:w-auto'>
                             <div className='relative overflow-x-auto overflow-y-auto bg-white max-h-80'>
-                                <table className=' text-lg text-gray-500 min-w-full w-full text-left rtl:text-right '>
-                                <thead className=' text-gray-700  bg-gray-50 text-center'>
+                                <table className=' text-sm md:text-lg text-gray-500 min-w-full w-full text-left rtl:text-right '>
+                                <thead className=' text-gray-700  bg-gray-50 text-left md:text-center'>
                                     <tr className=''>
-                                    <th scope='col' className='py-2 px-4'>No</th>
-                                    <th scope='col' className='py-2 px-4'>Nama</th>
-                                    <th scope='col' className='py-2 px-4'>NIM</th>
-                                    <th scope='col' className='py-2 px-4'>Jurusan</th>
-                                    <th scope='col' className='py-2 px-4'>Fakultas</th>
-                                    <th scope='col' className='py-2 px-4'>No Telepon</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2'>No</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2'>Nama</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2'>NIM</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2'>Jurusan</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2'>Fakultas</th>
+                                    <th scope='col' className='py-1 px-2 md:px-4 md:py-2s'>No Telepon</th>
                                     </tr>
                                 </thead>
-                                <tbody className='text-center'>
+                                <tbody className='text-left md:text-center'>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                     <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
-                                    </tr>
-                                    <tr>
-                                    <td scope='col' className='px-4'>1</td>
-                                    <td scope='col' className='px-4'>Muhamad Ramdan</td>
-                                    <td scope='col' className='px-4'>1207050179</td>
-                                    <td scope='col' className='px-4'>Teknik Informatika</td>
-                                    <td scope='col' className='px-4'>Sains dan Teknologi</td>
-                                    <td scope='col' className='px-4'>089647264291</td>
+                                      <td scope='col' className='px-2 md:px-4'>1</td>
+                                      <td scope='col' className='px-2 md:px-4'>Muhamad Ramdan</td>
+                                      <td scope='col' className='px-2 md:px-4'>1207050179</td>
+                                      <td scope='col' className='px-2 md:px-4'>Teknik Informatika</td>
+                                      <td scope='col' className='px-2 md:px-4'>Sains dan Teknologi</td>
+                                      <td scope='col' className='px-2 md:px-4'>089647264291</td>
                                     </tr>
                                 </tbody>
                                 </table>
--- pages/dosen/detailLaporan/[id].js
+++ pages/dosen/detailLaporan/[id].js
@@ -22,27 +22,27 @@ export default function DetailLaporan() {
             <div className='absolute my-2 mx-4 md:my-6 md:mx-6 bg-white p-2 rounded-full drop-shadow-xl z-40'>
                 <Link href='/dosen/laporan' className="text-xl"><IoChevronBackOutline /></Link>
             </div>
-            <Navbar />
-            <div className="mt-14 mb-2 ml-6 md:ml-10 md:mt-6 md:mb-10 font-bold text-2xl md:text-5xl text-white flex justify-center">
+            {/* <Navbar /> */}
+            <div className="mt-12 mb-6 ml-6 md:ml-10 md:mt-10 md:mb-4 font-bold text-3xl md:text-5xl text-white flex justify-center">
               <h1>Laporan Kelompok</h1>
             </div>
             <div className="flex justify-center items-center">
-            <div className="p-3 mx-4 md:mt-8 md:mx-20 md:p-6 bg-iceGray rounded-xl md:w-full">
-                <h1 class="text-2xl font-bold text-gray-900 dark:text-white flex justify-center items-center">
+            <div className="p-3 mx-6 md:mt-4 md:mx-20 md:p-4 bg-iceGray rounded-xl md:w-1/2">
+                <h1 class="text-lg md:text-2xl font-bold text-gray-900 dark:text-white flex justify-center items-center">
                     {namakelompok}
                 </h1>
                 <div className="md:flex md:justify-center md:items-center">
                     <div class=" pb-2 text-left">
-                        <div className="mb-2">
+                        <div className="mb-2 text-sm font-semibold md:font-semibold md:text-lg">
                             <h3>Ketua : {ketua}</h3>
                             <h3>Kontak Ketua : {telp}</h3>
                             <h3>Lokasi : Desa Cipaku, Kecamatan Paseh, Kabupaten Bandung, Jawa Barat</h3>
                             <h3>Anggota :</h3>
                         </div>
-                        <div className='bg-white grid justify-center items-center rounded-lg md:mx-9 md:w-full'>
-                            <div className='relative overflow-x-auto overflow-y-auto bg-white max-h-80'>
-                                <table className=' text-lg text-gray-500 min-w-full w-full text-left rtl:text-right '>
-                                <thead className=' text-gray-700 text-center'>
+                        <div className=' mx-2 md:mx-4 md:w-auto'>
+                            <div className='relative overflow-x-auto overflow-y-auto bg-white md:w-full max-h-80'>
+                                <table className=' text-sm md:text-lg text-gray-500 min-w-full w-full text-left rtl:text-right '>
+                                <thead className=' text-gray-700 bg-gray-50 text-left md:text-center'>
                                     <tr className=''>
                                     <th scope='col' className='py-2 px-4'>No</th>
                                     <th scope='col' className='py-2 px-4'>Jenis Laporan</th>
@@ -50,7 +50,7 @@ export default function DetailLaporan() {
                                     <th scope='col' className='py-2 px-4'>Actions</th>
                                     </tr>
                                 </thead>
-                                <tbody className='text-center'>
+                                <tbody className='text-left md:text-center'>
                                     <tr>
                                         <td scope='col' className='px-4'>1</td>
                                         <td scope='col' className='px-4'>Logbook</td>
@@ -74,7 +74,7 @@ export default function DetailLaporan() {
                                     </tr>
                                     <tr>
                                         <td scope='col' className='px-4'>1</td>
-                                        <td scope='col' className='px-4'>Logbook</td>
+                                        <td scope='col' className='px-4'>Artikel</td>
                                         <td scope='col' className='px-4'>https:\\</td>
                                         <td scope='col' className='px-4'>
                                             <div className='space-x-2'>
